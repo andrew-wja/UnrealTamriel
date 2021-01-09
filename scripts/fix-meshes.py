d5322259d9f657e53591714e2b003e2ba07ee80b
Amy Molloy <amypotato@icloud.com>
2021-01-09 18:18:09 +0000

    Attempt to bake textures to new combined texture

--- scripts/fix-meshes.py
+++ scripts/fix-meshes.py
@@ -10,7 +10,8 @@ for root, dirs, files in os.walk(path):
       out_file = os.path.join("D:\\UnrealTamriel\\Data\\meshes\\x\\output",f)
       fbx_out = out_file + '.fbx'
       
-      export_fbx = True
+      export_nif = False
+      export_fbx = False
       
       if bpy.context.object:
           bpy.ops.object.mode_set(mode='OBJECT')
@@ -27,6 +28,9 @@ for root, dirs, files in os.walk(path):
 
       for block in bpy.data.images:
             bpy.data.images.remove(block)
+            
+      for block in bpy.data.node_groups:
+            bpy.data.node_groups.remove(block)
       try:
         if True:
             print("Converting {}...".format(mesh_file))
@@ -37,7 +41,7 @@ for root, dirs, files in os.walk(path):
                 print('Mesh with invalid start node found: {}', f)
                 
                 rootTri = next((obj for obj in bpy.data.objects if obj.name.startswith('Tri Ex')), None)
-                incorrectRootTri = next((obj for obj in bpy.data.objects if obj.name.startswith('Tri Tri Ex')), None)
+                incorrectRootTri = rootTri.children[0]
                 sceneRoot = next((obj for obj in bpy.data.objects if obj.name.startswith('Ex')), None)
                 
                 print('current rootTri: {}:', rootTri)
@@ -63,11 +67,17 @@ for root, dirs, files in os.walk(path):
                 
                 
                 if True:
+                    print('Attempting to merge textures')
                     bpy.ops.object.select_all(action='DESELECT')
                     bpy.context.scene.render.engine = 'CYCLES'
                     
                     rootTri = next((obj for obj in bpy.data.objects if obj.name.startswith('Tri Ex')), None)
                     rootTri.children[0].select_set(True)
+                    rootTri.children[0].data.uv_layers.new(name='new')
+                    rootTri.children[0].data.uv_layers['new'].active = True
+                    
+                    bpy.ops.object.mode_set(mode='EDIT')
+                    bpy.ops.uv.unwrap()
                     
                     obj = bpy.context.active_object
                     print('Selected: {}', obj.name)
@@ -76,6 +86,8 @@ for root, dirs, files in os.walk(path):
                     # You can choose your texture size (This will be the de bake image)
                     image_name = obj.name + '_BakedTexture'
                     img = bpy.data.images.new(image_name,1024,1024)
+                    
+                    uv = bpy.data
                     
                     #Due to the presence of any multiple materials, it seems necessary to iterate on all the materials, and assign them a node + the image to bake.
                     for mat in obj.data.materials:
@@ -89,19 +101,28 @@ for root, dirs, files in os.walk(path):
                         texture_node.image = img #Assign the image to the node
                         
                     bpy.context.view_layer.objects.active = obj
-                    bpy.ops.object.bake(type='DIFFUSE', save_mode='EXTERNAL')
+                    bpy.ops.object.bake(type='COMBINED', save_mode='EXTERNAL')
 
-                    img.save_render(filepath='C:\\TEMP\\baked.png')
-                        
+                    img.save_render(filepath="D:\\UnrealTamriel\\Data\\textures\\output" + f + ".dds")
+                    
+                    
+                    bpy.ops.object.mode_set(mode='OBJECT')
                     #In the last step, we are going to delete the nodes we created earlier
                     for mat in obj.data.materials:
                         for n in mat.node_tree.nodes:
-                            if n.name == 'Bake_node':
+                            if True and n.name == 'Bake_node':
                                 mat.node_tree.nodes.remove(n)
+                        bpy.ops.object.select_all(action='DESELECT')
+                        obj.data.materials.remove(mat)
+                        bpy.data.materials.remove(mat)
                         
+                    #newMat = obj.data.materials.new()
+                    #newMat.
                 
-                bpy.ops.export_scene.mw(filepath=out_file)
-                bpy.ops.export_scene.fbx(filepath=fbx_out)
+                if export_nif:
+                    bpy.ops.export_scene.mw(filepath=out_file)
+                if export_fbx:
+                    bpy.ops.export_scene.fbx(filepath=fbx_out)
       except Exception as e:
         print("Corrupted NIF file: {}".format(mesh_file))
         print(e)
