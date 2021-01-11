589ca7c78fa942a9340cec8c0cb83469cdf7108d
Andrew <aanderso@tcd.ie>
2021-01-11 10:44:29 +0000

    Fix selected to active issue and make sure we don't accidentally re-process already processed files

--- scripts/fix-meshes.py
+++ scripts/fix-meshes.py
@@ -34,11 +34,16 @@ parser.add_argument('--python', type=str, required=False,
 
 def main():
     args = parser.parse_args()
+
+    processed = set()
+
     for root, dirs, files in os.walk(args.asset_path):
       for f in files:
-        if f.endswith('.nif'):
+        if f.endswith('.nif') and not f in processed:
+          processed.add(f)
+
           in_file = os.path.join(root, f)
-          out_file = os.path.join(root, os.path.splitext(f)[0] + '.' + str(args.mesh_format))
+          out_file = os.path.join(root, os.path.splitext(f)[0] + '.converted.' + str(args.mesh_format))
 
           if bpy.context.object:
               bpy.ops.object.mode_set(mode='OBJECT')
@@ -59,7 +64,6 @@ def main():
           for block in bpy.data.node_groups:
                 bpy.data.node_groups.remove(block)
           try:
-            print("Converting {}...".format(in_file))
             bpy.ops.import_scene.mw(filepath=in_file)
             bpy.ops.object.select_all(action='DESELECT')
 
@@ -70,9 +74,9 @@ def main():
                 incorrectRootTri = rootTri.children[0]
                 sceneRoot = next((obj for obj in bpy.data.objects if obj.name.startswith('Ex')), None)
 
-                # ~ print('current rootTri: {}:', rootTri)
-                # ~ print('incorrect rootTri: {}:', incorrectRootTri)
-                # ~ print('sceneRoot: {}', sceneRoot)
+                print('current rootTri: {}:', rootTri)
+                print('incorrect rootTri: {}:', incorrectRootTri)
+                print('sceneRoot: {}', sceneRoot)
 
                 for obj in incorrectRootTri.children:
                     obj.parent = rootTri
@@ -81,85 +85,88 @@ def main():
                 incorrectRootTri.select_set(True)
                 bpy.ops.object.delete()
 
-                print('Attempting to merge meshes')
-                for obj in bpy.data.objects:
-                    if obj.name.startswith('Tri'):
-                        obj.select_set(True)
-                        if obj.name.endswith('0'):
-                            bpy.context.view_layer.objects.active = obj
+            print('Attempting to merge meshes')
+            for obj in bpy.data.objects:
+                if obj.name.startswith('Tri'):
+                    obj.select_set(True)
+                    if obj.name.endswith('0'):
+                        bpy.context.view_layer.objects.active = obj
 
-                bpy.ops.object.join()
+            bpy.ops.object.join()
 
-                print('Attempting to merge textures')
+            print('Attempting to merge textures')
+            bpy.ops.object.select_all(action='DESELECT')
+            bpy.context.scene.render.engine = 'CYCLES'
+
+            rootTri = next((obj for obj in bpy.data.objects if obj.name.startswith('Tri Ex')), None)
+            print('rootTri: {}', rootTri)
+            print('rootTri Type: {}', rootTri.type)
+
+            if (rootTri.type != 'MESH'):
+                rootTri = rootTri.children[0]
+            rootTri.select_set(True)
+            rootTri.data.uv_layers.new(name='new')
+            rootTri.data.uv_layers['new'].active = True
+
+            bpy.ops.object.mode_set(mode='EDIT')
+            bpy.ops.uv.unwrap()
+
+            obj = bpy.context.active_object
+            print('Selected: {}', obj.name)
+            print('Selected Type: {}', obj.type)
+
+            # You can choose your texture size (This will be the de bake image)
+            image_name = obj.name + '_BakedTexture'
+            img = bpy.data.images.new(image_name,1024,1024)
+
+            #Due to the presence of any multiple materials, it seems necessary to iterate on all the materials, and assign them a node + the image to bake.
+            for mat in obj.data.materials:
+                print('adding node to material: {}', mat.name)
+                mat.use_nodes = True #Here it is assumed that the materials have been created with nodes, otherwise it would not be possible to assign a node for the Bake, so this step is a bit useless
+                nodes = mat.node_tree.nodes
+                texture_node = nodes.new('ShaderNodeTexImage')
+                texture_node.name = 'Bake_node'
+                texture_node.select = True
+                nodes.active = texture_node
+                texture_node.image = img #Assign the image to the node
+
+            bpy.context.view_layer.objects.active = obj
+            bpy.ops.object.bake(type='DIFFUSE', pass_filter={'COLOR'}, save_mode='EXTERNAL', use_selected_to_active=True)
+
+            if args.texture_format == TextureFormat.bmp:
+                img.save_render(filepath=os.path.join(root, os.path.splitext(f)[0] + ".bmp"))
+            elif args.texture_format == TextureFormat.dds:
+                img.save_render(filepath=os.path.join(root, os.path.splitext(f)[0] + ".dds"))
+            elif args.texture_format == TextureFormat.tga:
+                img.save_render(filepath=os.path.join(root, os.path.splitext(f)[0] + ".tga"))
+            else:
+                print("Unrecognized texture format requested: " + str(args.texture_format))
+
+            bpy.ops.object.mode_set(mode='OBJECT')
+            #In the last step, we are going to delete the nodes we created earlier
+            for mat in obj.data.materials:
+                for n in mat.node_tree.nodes:
+                    if True and n.name == 'Bake_node':
+                        mat.node_tree.nodes.remove(n)
                 bpy.ops.object.select_all(action='DESELECT')
-                bpy.context.scene.render.engine = 'CYCLES'
+                bpy.data.materials.remove(mat)
 
-                rootTri = next((obj for obj in bpy.data.objects if obj.name.startswith('Tri Ex')), None)
-                print('rootTri: {}', rootTri)
-                print('rootTri Type: {}', rootTri.type)
+            newMat = bpy.data.materials.new(os.path.splitext(f)[0])
+            bpy.ops.material.mw_create_shader()
+            print(dir(newMat))
 
-                if (rootTri.type != 'MESH'):
-                    rootTri = rootTri.children[0]
-                rootTri.select_set(True)
-                rootTri.data.uv_layers.new(name='new')
-                rootTri.data.uv_layers['new'].active = True
+            # We need to re-wrap the UVs so we can export the whole shebang
+            bpy.ops.object.mode_set(mode='EDIT')
+            bpy.ops.uv.wrap()
 
-                bpy.ops.object.mode_set(mode='EDIT')
-                bpy.ops.uv.unwrap()
-
-                obj = bpy.context.active_object
-                print('Selected: {}', obj.name)
-                print('Selected Type: {}', obj.type)
-
-                # You can choose your texture size (This will be the de bake image)
-                image_name = obj.name + '_BakedTexture'
-                img = bpy.data.images.new(image_name,1024,1024)
-
-                #Due to the presence of any multiple materials, it seems necessary to iterate on all the materials, and assign them a node + the image to bake.
-                for mat in obj.data.materials:
-                    print('adding node to material: {}', mat.name)
-                    mat.use_nodes = True #Here it is assumed that the materials have been created with nodes, otherwise it would not be possible to assign a node for the Bake, so this step is a bit useless
-                    nodes = mat.node_tree.nodes
-                    texture_node = nodes.new('ShaderNodeTexImage')
-                    texture_node.name = 'Bake_node'
-                    texture_node.select = True
-                    nodes.active = texture_node
-                    texture_node.image = img #Assign the image to the node
-
-                bpy.context.view_layer.objects.active = obj
-                bpy.ops.object.bake(type='DIFFUSE', pass_filter={'COLOR'}, save_mode='EXTERNAL')
-
-                if args.texture_format == TextureFormat.bmp:
-                    img.save_render(filepath=os.path.join(root, os.path.splitext(f)[0] + ".bmp"))
-                elif args.texture_format == TextureFormat.dds:
-                    img.save_render(filepath=os.path.join(root, os.path.splitext(f)[0] + ".dds"))
-                elif args.texture_format == TextureFormat.tga:
-                    img.save_render(filepath=os.path.join(root, os.path.splitext(f)[0] + ".tga"))
-                else:
-                    print("Unrecognized texture format requested: " + str(args.texture_format))
-
-                bpy.ops.object.mode_set(mode='OBJECT')
-                #In the last step, we are going to delete the nodes we created earlier
-                for mat in obj.data.materials:
-                    for n in mat.node_tree.nodes:
-                        if True and n.name == 'Bake_node':
-                            mat.node_tree.nodes.remove(n)
-                    bpy.ops.object.select_all(action='DESELECT')
-                    bpy.data.materials.remove(mat)
-
-                newMat = bpy.data.materials.new(os.path.splitext(f)[0])
-                bpy.ops.material.mw_create_shader()
-                print(dir(newMat))
-
-
-                if args.mesh_format == MeshFormat.nif:
-                    bpy.ops.export_scene.mw(filepath=out_file)
-                elif args.mesh_format == MeshFormat.fbx:
-                    bpy.ops.export_scene.fbx(filepath=out_file)
-                elif args.mesh_format == MeshFormat.obj:
-                    bpy.ops.export_scene.obj(filepath=out_file)
-                else:
-                    print("Unrecognized mesh format requested: " + str(args.mesh_format))
+            if args.mesh_format == MeshFormat.nif:
+                bpy.ops.export_scene.mw(filepath=out_file)
+            elif args.mesh_format == MeshFormat.fbx:
+                bpy.ops.export_scene.fbx(filepath=out_file)
+            elif args.mesh_format == MeshFormat.obj:
+                bpy.ops.export_scene.obj(filepath=out_file)
+            else:
+                print("Unrecognized mesh format requested: " + str(args.mesh_format))
 
           except Exception as e:
             print("Corrupted NIF file: {}".format(in_file))
