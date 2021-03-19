841b0d243e98044a9426baecc659d8d615ddee7b
Andrew <aanderso@tcd.ie>
2021-03-19 14:36:39 +0000

    Fix

--- scripts/convert-dds-to-tga.sh
+++ scripts/convert-dds-to-tga.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
 base=$(dirname "$1")/$(basename "$1" .dds)
-
-if [[ ! -f $base.tga ]]
-then
-  echo "Converting $base.dds"
-  convert "$base.dds" "$base.tga"
-fi
+echo "Converting $base.dds"
+convert "$base.dds" "${base}.tga"
+ln -sf "${base}.tga" "${base^}.tga" # for NifSkope
